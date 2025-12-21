cb952c9331de66ca9759e8a0ae153dbfc20d00d9
陸楷軒 <mavericklook03@gmail.com>
2025-12-21 21:32:45 +0800

    console.log("API_BASE_URL =", process.env.REACT_APP_API_URL);

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 // Base URL - change to your backend URL
+console.log("API_BASE_URL =", process.env.REACT_APP_API_URL);
+
 const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
 
 // Create axios instance
